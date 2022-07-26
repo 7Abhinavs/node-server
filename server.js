2456c504cb698c3876f3857e41f0ff41973121ce
7Abhinavs <abhinav.20m0141045@abes.ac.in>
2022-07-26 12:01:41 +0530

    7 commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var handleRequest = function (request, response) {
   response.writeHead(200);
   
-  response.end('Welcome, Its Blue Green Deployment!!');
+  response.end('Welcome, Its Blue Green Deployment!!!');
   
 };
 var www = http.createServer(handleRequest);
